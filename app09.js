dba2a22d4499d71c5f796e66a9fac24c384c1711
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-09-13 10:31:10 +0530

    changes

--- app09.js
+++ app09.js
@@ -1,15 +1,36 @@
-var person = new Object();
-
-person['firstname'] = 'John';
-person['lastname'] = 'Doe';
-
+var person = {}; // object literal notation
 console.log(person);
-console.log(person.firstname);
-console.log(person['firstname']);
 
-person.address = new Object();
-person.address.street = '111 Main Street';
-person.address.state = 'NY';
-person.address.city = 'NY';
-
+person = { firstName: 'Max', lastName: 'Wilson' };
 console.log(person);
+
+var john = {
+  firstname: 'John',
+  lastname: 'Smith',
+  address: {
+    street: '111 main street',
+    city: 'NY',
+    state: 'NY',
+  },
+};
+console.log(john);
+
+function greet(person) {
+  console.log('Hi ' + person.firstname + ' ' + person.lastname);
+}
+
+greet(john);
+greet('X');
+
+greet({
+  firstname: 'Mary',
+  lastname: 'Smith',
+});
+
+console.log(john);
+console.log(JSON.stringify(john));
+
+var jsonstring =
+  '{"firstname":"Max","lastname":"Johnson","address":{"street":"111 main street","city":"NY","state":"NY"}}';
+var max = JSON.parse(jsonstring);
+console.log(max);
